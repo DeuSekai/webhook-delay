d044a10bfd311a7040e2159c84049d515cb7f9af
David Arturo Ramírez Guerrero <39871892+DeuSekai@users.noreply.github.com>
2018-12-11 10:33:48 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,29 +1,28 @@
 <?php
 
-$method = $_SERVER['REQUEST_METHOD'];
+function sendMessage($parameters) {
+    echo json_encode($parameters);
+}
 
-// Process only when method is POST
-if ($method == 'POST') {
-    
-    $requestBody = file_get_contents('php://input');
-    $json        = json_decode($requestBody);
-    
-    $speech = NULL;
-            
-    $response              = new \stdClass();
-    $response->speech      = $speech;
-    $response->displayText = $speech;
-    $response->source      = "webhook";
-    
+$update_response = file_get_contents("php://input");
+$update          = json_decode($update_response, true);
+
     //4 seconds delay
     sleep(4);
-    
-    echo json_encode($response);
-    
-} else {
-    
-    echo "Method not allowed";
-    
-}
+    sendMessage(array(
+            "source" => $update["responseId"],
+            "fulfillmentText"=>NULL,
+            "payload" => array(
+                "items"=>[
+                    array(
+                        "simpleResponse"=>
+                    array(
+                        "textToSpeech"=>"Bad request"
+                         )
+                    )
+                ],
+                ),
+           
+        ));
 
 ?>
